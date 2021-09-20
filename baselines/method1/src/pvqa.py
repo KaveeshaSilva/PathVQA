c04879bf0a42a253ef9413d9e622bd48519000ac
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2021-09-20 17:24:05 +0530

    save dir issue - add running_loss

--- baselines/method1/src/pvqa.py
+++ baselines/method1/src/pvqa.py
@@ -91,6 +91,7 @@ class PVQA:
         best_valid = 0.
         lastEpoch = -1  # self.getLastEpoch()
         start_epoch = lastEpoch + 1  # new
+        running_loss = 0
 
         print('jijijijijij')
         for epoch in range(start_epoch, args.epochs):
@@ -235,45 +236,6 @@ class PVQA:
 
 
 if __name__ == '__main__':
-    import torch
-    import torch.nn as nn
-    import torch.optim as optim
-
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 6, 5)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
-
-        def forward(self, x):
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 16 * 5 * 5)
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
-            return x
-
-    net = Net()
-
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    EPOCH = 5
-    PATH = "/content/drive/My Drive/model000.pt"
-    LOSS = 0.4
-
-    torch.save({
-        'epoch': EPOCH,
-        'model_state_dict': net.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': LOSS,
-    }, PATH)
-
-    print('saveddddd  test model')
 
     pvqa = PVQA()
     print('first')
