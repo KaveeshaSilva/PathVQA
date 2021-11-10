90eeb649b1dda36a3ca5e09ab62f127552b0d243
unknown <thanujamaheepala@gmail.com>
2021-11-10 12:25:52 +0530

    simulator file added

---
+++ baselines/method1/src/pvqa_simulate.py
@@ -0,0 +1,96 @@
+# coding=utf-8
+
+import base64
+import json
+import os
+import pandas as pd
+import pickle
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from param import args
+from utils import load_obj_tsv
+
+import re
+from sklearn.metrics import f1_score
+from nltk.translate.bleu_score import sentence_bleu
+
+from tasks.pvqa_model import PVQAModel
+
+baseUrl = 'drive/MyDrive/PathVQA/'
+model_dir = baseUrl+"/checkpointtemp_LXRT.pth"
+
+
+FIELDNAMES = ['image_id', 'image_w', 'image_h',
+              'num_boxes', 'boxes', 'features']
+
+
+def load_tsv(split: str):
+    tsv_file = baseUrl+'data/pvqa/images/%s%s.csv' % (split, args.pvqaimgv)
+    df = pd.read_csv(tsv_file, delimiter='\t', names=FIELDNAMES)
+
+    data = []
+    for i in range(df.shape[0]):
+        datum = {}
+        datum['img_id'] = '%s_%04d' % (split, df['image_id'][i])
+        datum['img_w'] = df['image_w'][i]
+        datum['img_h'] = df['image_h'][i]
+        datum['num_boxes'] = df['num_boxes'][i]
+
+        boxes = df['boxes'][i]
+        buf = base64.b64decode(boxes[1:])
+        temp = np.frombuffer(buf, dtype=np.float64).astype(np.float32)
+        datum['boxes'] = temp.reshape(datum['num_boxes'], -1)
+
+        features = df['features'][i]
+        buf = base64.b64decode(features[1:])
+        temp = np.frombuffer(buf, dtype=np.float32)
+        datum['features'] = temp.reshape(datum['num_boxes'], -1)
+
+        data.append(datum)
+
+    return data
+
+splits = ['test']
+# loading detection features to img_data
+imgid2img = {}
+for split in splits:
+    data = load_tsv(split)
+
+    for datum in data:
+        imgid2img[datum['img_id']] = datum
+
+label2ans = pickle.load(open(baseUrl+'data/pvqa/qas/trainval_label2ans.pkl', 'rb'))
+
+model = PVQAModel(4092)
+state_dict = torch.load("%s.pth" % model_dir)
+model.load_state_dict(state_dict["model_state_dict"])
+
+while True:
+    print('Enter image name (ex:- test_0001):')
+    img_id = input().strip()
+
+    image_fet = imgid2img[img_id]
+    feats = image_fet['features']
+    boxes = image_fet['boxes']
+
+    x = 'y'
+
+    while (x in ['y','Y']):
+        print('Enter a question:')
+        q = input().strip()
+
+        #predict and print max 5
+        with torch.no_grad():
+            feats, boxes = feats.cuda(), boxes.cuda()
+
+            logit = model(feats, boxes, q, "xxx")
+            score, label = logit.max(1)
+        
+        print(label2ans[label]+" - "+score)
+
+        print('Question about previous image? (y/n)')
+        x = input().strip()
+
