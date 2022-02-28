b8daa16de0dfccb4030fc5c8cd98f6e12921906c
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2022-02-28 20:51:19 +0530

    discriminator lr reset

--- baselines/method1/src/pvqa_adv_auto.py
+++ baselines/method1/src/pvqa_adv_auto.py
@@ -132,7 +132,7 @@ class PVQAAdv:
             self.optimizer_G = torch.optim.Adam(
                 self.q_i_model.parameters(), lr=5e-5, betas=(0.5, 0.999))
             self.optimizer_D = torch.optim.Adam(
-                self.discriminator.parameters(), lr=5e-5, betas=(0.5, 0.999))
+                self.discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
         else:
             self.optimizer_G = checkpoint['saved_optimizer_G']
             self.optimizer_D = checkpoint['saved_optimizer_D']
