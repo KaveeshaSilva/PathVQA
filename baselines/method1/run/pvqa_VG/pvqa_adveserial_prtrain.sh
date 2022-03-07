1f466f823bd99f18c8cfbb36b9eb97d4e2424662
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2022-03-07 15:32:09 +0530

    lxmert_adv_pretrain - minor issue

--- baselines/method1/run/pvqa_VG/pvqa_adveserial_prtrain.sh
+++ baselines/method1/run/pvqa_VG/pvqa_adveserial_prtrain.sh
@@ -19,7 +19,7 @@ do
       --taskQA_woi --taskMatched --taskVA2 \
       --visualLosses obj,attr,feat \
       --wordMaskRate 0.15 --objMaskRate 0.15 \
-      --train pvqa_train --pvqa_valid val \
+      --train pvqa_train --valid pvqa_val \
       --loadLXMERT ${baseUrl}/baselines/method1/snap/pretrained/model \
       --llayers 9 --xlayers 5 --rlayers 5 \
       --batchSize 32 --optim bert --lr 1e-4 --epochs 20 \
@@ -32,31 +32,13 @@ do
   ft_output=snap/pvqa/$ft_name
   mkdir -p $ft_output
 
-  # See Readme.md for option details.
-  CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
-      python ${baseUrl}/baselines/method1/src/lxmert_adv_pretrain.py \
-      --train train --valid val  \
-      --llayers 9 --xlayers 5 --rlayers 5 \
-      --loadLXMERT ${pre_output}/Epoch01 \
-      --batchSize 32 --optim bert --lr 5e-5 --epochs 120 \
-      --seed $seed --pvqaimgv $imgv \
-      --tqdm --output $ft_output
-
-    # The name of this experiment.
+  # The name of this experiment.
   res_name=pvqa_pre_from_pretrained_${dir}_${seed}_lxr955_results
 
   # Save logs and models under snap/vqa; make backup.
   res_output=snap/pvqa/$res_name
   mkdir -p $res_output
 
-  # See Readme.md for option details.
-  CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
-      python ${baseUrl}/baselines/method1/src/lxmert_adv_pretrain.py \
-      --test test  --train val --valid "" \
-      --load ${ft_output}/BEST \
-      --llayers 9 --xlayers 5 --rlayers 5 \
-      --batchSize 32 --optim bert --lr 5e-5 --epochs 4 \
-      --seed $seed --pvqaimgv $imgv \
-      --tqdm --output $res_output
+
 
 done
